d8d360aedb323ce047389e8e3cbdac522966f0f0
mosmart <msmart@berkeley.edu>
2016-10-23 18:09:14 -0700

    removed row index column from credit

--- code/scripts/data-processing-script.R
+++ code/scripts/data-processing-script.R
@@ -3,6 +3,9 @@
 # import dataset
 credit <- read.table('../../data/Credit.csv', header=TRUE, sep=',')
 
+# remove row index column
+credit <- credit[,-1]
+
 ###################
 
 # Create Dummy Variables
@@ -11,7 +14,7 @@ credit <- read.table('../../data/Credit.csv', header=TRUE, sep=',')
 temp_credit <- model.matrix(Balance ~ ., data = credit)
 
 # removing column of ones, and appending Balance
-new_credit <- cbind(temp_credit[ ,-1], Balance = Balance)
+new_credit <- cbind(temp_credit[ ,-1], Balance = credit$Balance)
 
 ###################
 
@@ -21,4 +24,4 @@ new_credit <- cbind(temp_credit[ ,-1], Balance = Balance)
 scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
 
 # export scaled data
-write.csv(scaled_credit, file = "path/of/processed/data/scaled-credit.csv")
+write.csv(scaled_credit, file = "../../data/scaled-credit.csv")
