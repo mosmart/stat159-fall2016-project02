71f790ed6bc32c5c4b78b5b2a08e72876fa12168
Lauren Hanlon <laurenmhanlon@gmail.com>
2016-10-31 01:16:10 -0700

    changed variable names to be more descriptive (fit to osl_fit)

--- code/scripts/osl-regression.R
+++ code/scripts/osl-regression.R
@@ -5,23 +5,23 @@ credit_scaled <- read.table('../../data/scaled-credit.csv', header=TRUE, sep=','
 
 # multi linear regression
 
-fit <- lm(Balance ~., data=credit_scaled)
-summary_fit <- summary(fit)
+osl_fit <- lm(Balance ~., data=credit_scaled[,-c(1,3:4)])
+osl_summary_fit <- summary(osl_fit)
 
 ## Residual Plot
 png('../../images/osl-residual-plot.png')
-plot(fit, which=1)
+plot(osl_fit, which=1)
 dev.off()
 
 ## Scale Location Plot
 png('../../images/osl-scale-location.png')
-plot(fit, which=3)
+plot(osl_fit, which=3)
 dev.off()
 
 ## Normal QQ Plot
 png("../../images/osl-normal-qq-plot.png")
-plot(fit, which=2)
+plot(osl_fit, which=2)
 dev.off()
 
 # save objects to file
-save(fit, summary_fit, file = "../../data/osl.RData")
+save(osl_fit, osl_summary_fit, file = "../../data/osl.RData")
