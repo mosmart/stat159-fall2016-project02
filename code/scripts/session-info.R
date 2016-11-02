00f7ba09424aae4c7f5304dc5ccf023ef75e638f
Lauren Hanlon <laurenmhanlon@gmail.com>
2016-11-01 22:16:28 -0700

    new session info script

---
+++ code/scripts/session-info.R
@@ -0,0 +1,10 @@
+# include all R packages in your project
+library(stargazer)
+library(testthat)
+library(glmnet)
+library(pls)
+library(ggplot2)
+sink("../../session-info.txt", append = TRUE)
+cat("Session Information\n\n")
+print(sessionInfo())
+sink()
